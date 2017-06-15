791a5134ecb496fd69368a2bf86a8b6e9c08c53e
Henny <pprochniak@sointeractive.pl>
2017-06-15 10:40:41 +0200

    Initial commit

---
+++ app/src/main/java/io/github/webbluetoothcg/bletestperipheral/AccelerometerServiceFragment.java
@@ -0,0 +1,20 @@
+package io.github.webbluetoothcg.bletestperipheral;
+
+import android.bluetooth.BluetoothGattService;
+import android.os.ParcelUuid;
+
+/**
+ * Created by Henny on 2017-06-15.
+ */
+
+public class AccelerometerServiceFragment extends ServiceFragment {
+    @Override
+    public BluetoothGattService getBluetoothGattService() {
+        return null;
+    }
+
+    @Override
+    public ParcelUuid getServiceUUID() {
+        return null;
+    }
+}
